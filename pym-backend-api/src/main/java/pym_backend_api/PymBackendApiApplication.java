d88691a748e88d5e628a7818137e1fc606872205
62294 <62294@etu.he2b.be>
2026-06-24 13:53:50 +0200

    init spring boot backend

---
+++ pym-backend-api/src/main/java/pym_backend_api/PymBackendApiApplication.java
@@ -0,0 +1,13 @@
+package pym_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymBackendApiApplication.class, args);
+	}
+
+}
